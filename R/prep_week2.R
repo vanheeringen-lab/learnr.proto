2bbcde5e649b7e390a68f47aa3d4ddc4e38e90df
siebrenf <siebrenf@gmail.com>
2020-12-04 18:34:11 +0100

    updated week2 to work with renv + bugfixes

--- R/prep_week2.R
+++ R/prep_week2.R
@@ -2,23 +2,27 @@ prep_week2 <- function(){
   #' prepare the .Rdata for week2
   #' object is saved in inst/extdata, which is installed to any device that installs the package
 
-  # generate all environmental variables to save
-  txdb_annotation <- readRDS("inst/extdata/week2/prepared_rds/txdb_annotation_chr19_grangeslist.rds")
-
-  load("inst/extdata/week2/prepared_rds/txdb_annotation_chr19_genes_tx_pr_tss_granges.RData") # loads genes, promoters_3k, promoters_def, transcripts
-
-  encode_ccres <- readRDS("inst/extdata/week2/prepared_rds/encode_ccres_chr19.rds")
-  encode_ccres_list <- split(encode_ccres, encode_ccres$ucsc_label)
-
-  entrezid_symbols <- readRDS("inst/extdata/week2/prepared_rds/gene_entrezids_genesymbols_df.rds")
-
+  # generate all variables that can/need to be accessible for every excersize
   monocytes_h3k4me3 <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547984.H3K4me3.bwa.GRCh38.20150527.chr19.bed", format = "narrowPeak")
+  monocytes_h3k4me1 <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547981.H3K4me1.bwa.GRCh38.broad.20150527.chr19.bed", format = "broadPeak")
+  monocytes_h3k9me3 <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547982.H3K9me3.bwa.GRCh38.broad.20150527.chr19.bed", format = "broadPeak")
+  monocytes_h3k27ac <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547980.H3K27ac.bwa.GRCh38.20150527.chr19.bed", format = "narrowPeak")
+  monocytes_h3k27me3 <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547983.H3K27me3.bwa.GRCh38.broad.20150527.chr19.bed", format = "broadPeak")
+  monocytes_h3k36me3 <- rtracklayer::import("inst/extdata/week2/blueprint/bed/C000S5H2.ERX547979.H3K36me3.bwa.GRCh38.broad.20150527.chr19.bed", format = "broadPeak")
 
-  # create the .Rdata object
-  save(txdb_annotation, genes, promoters_3k, promoters_def, transcripts, encode_ccres, encode_ccres_list, entrezid_symbols, monocytes_h3k4me3, file =  "inst/extdata/week2.Rdata")
+  # make a list of named GRanges objects
+  monocytes_list  <- GRangesList(monocytes_h3k4me1, monocytes_h3k4me3, monocytes_h3k9me3, monocytes_h3k27ac, monocytes_h3k27me3, monocytes_h3k36me3)
+  names(monocytes_list) <- c("h3k4me1", "h3k4me3", "h3k9me3", "h3k27ac", "h3k27me3", "h3k36me3")
+
+  # store the variables in an .Rdata object
+  save(
+    #txdb_annotation, genes, promoters_3k, promoters_def, transcripts, encode_ccres, encode_ccres_list, entrezid_symbols, monocytes_h3k4me3,
+    monocytes_h3k4me3, monocytes_h3k4me1, monocytes_h3k9me3, monocytes_h3k27ac, monocytes_h3k27me3, monocytes_h3k36me3, monocytes_list,
+    file =  "inst/extdata/week2.Rdata"
+  )
 
   # install the package
-  dev_path = "/home/siebrenf/git/edu/learnr.proto"
+  dev_path = "."  # should be something like "/home/siebrenf/git/edu/learnr.proto"
   devtools::install(
     pkg=dev_path,
     dependencies=F
